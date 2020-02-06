f683eb8f5766158b0138739d5f216e2253bc74f3
Przemyslaw Sadlo <przemyslaw.sadlo@pl.abb.com>
2020-02-06 14:27:01 +0100

    Creating world

---
+++ Engine2/Models/World.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class World
+    {
+        private List<Location> _locations = new List<Location>();
+
+        internal void AddLocation(int xCoordinate, int yCoordinate, string name, string description, string imageName)
+        {
+            Location loc = new Location();
+            loc.XCoordinate = xCoordinate;
+            loc.YCoordinate = yCoordinate;
+            loc.Name = name;
+            loc.Description = description;
+            loc.ImageName = imageName;
+
+            _locations.Add(loc);
+        }
+
+        public Location LocationAt (int xCoordinate, int yCoordinate) => _locations.FirstOrDefault(o => o.XCoordinate == xCoordinate && o.YCoordinate == yCoordinate);
+    }
+}

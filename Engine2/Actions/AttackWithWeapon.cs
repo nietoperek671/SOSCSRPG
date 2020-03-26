a85e0d6fa9677de071bbdbb468a6c431c86eeae3
Przemyslaw Sadlo <przemyslaw.sadlo@pl.abb.com>
2020-03-26 10:26:50 +0100

    12.6 refactor

--- Engine2/Actions/AttackWithWeapon.cs
+++ Engine2/Actions/AttackWithWeapon.cs
@@ -3,19 +3,17 @@ using System;
 
 namespace Engine.Actions
 {
-    public class AttackWithWeapon : IAction
+    public class AttackWithWeapon : BaseAction, IAction
     {
-        private readonly GameItem _weapon;
         private readonly int _minimumDamage;
         private readonly int _maximumDamage;
 
-        public event EventHandler<string> OnActionPerformed;
-
-        public AttackWithWeapon(GameItem weapon, int minimumDamage, int maximumDamage)
+        public AttackWithWeapon(GameItem itemInUse, int minimumDamage, int maximumDamage)
+            : base(itemInUse)
         {
-            if (weapon.Category != GameItem.ItemCategory.Weapon)
+            if (itemInUse.Category != GameItem.ItemCategory.Weapon)
             {
-                throw new ArgumentException($"{weapon.Name} is not a weapon");
+                throw new ArgumentException($"{itemInUse.Name} is not a weapon");
             }
 
             if (minimumDamage < 0)
@@ -28,7 +26,6 @@ namespace Engine.Actions
                 throw new ArgumentException("minimum damage > maximum damage");
             }
 
-            _weapon = weapon;
             _minimumDamage = minimumDamage;
             _maximumDamage = maximumDamage;
         }
@@ -49,11 +46,6 @@ namespace Engine.Actions
                 ReportResult($"{actorName} hit {targetName} for {damage} point{(damage > 1 ? "s" : "")}");
                 target.TakeDamage(damage);
             }
-        }
-
-        private void ReportResult(string result)
-        {
-            OnActionPerformed?.Invoke(this, result);
         }
     }
 }
